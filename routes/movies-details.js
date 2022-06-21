517cb6d9bed856229e01d9347364ab9a74a2d3e9
Syed Wajahat Ali <wajahat.klk25.com>
2022-06-21 17:28:00 +0200

    getting ERROR

--- routes/movies-details.js
+++ routes/movies-details.js
@@ -3,10 +3,11 @@ const Movie = require('../models/Movie');
 const router = express.Router();
 
 router.get('/movie/:movieId', async(req, res, next) =>{
-    const {movieId} = req.body;
+    const {movieId} = req.params;
     try {
-        const movie = await Movie.findById(movieId);
+        const movie = await movies.findById(movieId);
         res.render('movies-details', movie)
+        res.json(movie)
     } catch (error) {
         next(error)
     }
